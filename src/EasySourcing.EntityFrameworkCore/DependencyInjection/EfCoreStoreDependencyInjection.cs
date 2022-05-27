abc35e9c6cf5f5f20518d337b36e8bd9ef5af012
BRUCE.L <bruce@sjfood.us>
2022-05-26 17:12:15 -0700

    Cleanup

--- src/EasySourcing.EntityFrameworkCore/DependencyInjection/EfCoreStoreDependencyInjection.cs
+++ src/EasySourcing.EntityFrameworkCore/DependencyInjection/EfCoreStoreDependencyInjection.cs
@@ -6,16 +6,6 @@ namespace EasySourcing.EntityFrameworkCore.DependencyInjection;
 
 public static class EfCoreStoreDependencyInjection
 {
-    public static IEventSourcingBuilder AddEfCoreStore<TEventSourcingDbContext>(
-        this IEventSourcingBuilder builder)
-        where TEventSourcingDbContext : DbContext, IEventSourcingDbContext
-    {
-        builder.Services.AddScoped<IEventStore, EventStore<TEventSourcingDbContext>>();
-        builder.Services.AddScoped<IMementoStore, MementoStore<TEventSourcingDbContext>>();
-
-        return builder;
-    }
-
     public static IEventSourcingBuilder UseEfCoreStore<TEventSourcingDbContext>(
         this IEventSourcingBuilder builder) where TEventSourcingDbContext : DbContext, IEventSourcingDbContext
     {
